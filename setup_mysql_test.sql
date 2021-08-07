5dde011e0e3c11e930a1f41e13cb672e519a30eb
Miguel22247 <nr2862002@gmail.com>
2021-08-07 19:55:37 -0300

    fixed some things

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,8 @@
 -- script that prepares MySQL server
 -- for the project
-CREATE DATABASE IF NOT EXISTS `hbnb_test_db`
-CREATE USER IF NOT EXISTS `hbnb_test`
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+USE hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
