f3bbd39141bf7ef0d58a160a09878c57ed2991df
Miguel22247 <nr2862002@gmail.com>
2021-08-08 20:06:17 -0300

    added testpep8

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
 """ Module for testing db storage """
+import unittest
+import pep8
+import inspect
+
+
+class Test_pep8(unittest.TestCase):
+    """pep8 test cases class"""
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/engine/db_storage.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
