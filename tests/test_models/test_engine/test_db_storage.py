81776ed13932ab48b1c9819274afd449dbade01e
Miguel22247 <nr2862002@gmail.com>
2021-08-08 20:07:26 -0300

    added all imports

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
 """ Module for testing db storage """
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.engine.db_storage import DBStorage
+from models.engine.file_storage import FileStorage
+import MySQLdb
 import unittest
 import pep8
-import inspect
+import os
+from os import getenv
+from models.base_model import BaseModel
+from models.user import User
 
 
 class Test_pep8(unittest.TestCase):
