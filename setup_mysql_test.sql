7c5d7b4e7811b256b42b4ed8c0b9586165c8fd17
Miguel22247 <nr2862002@gmail.com>
2021-08-08 09:34:56 -0300

    changed SQL files

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -5,4 +5,3 @@ USE hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
-FLUSH PRIVILEGES;
