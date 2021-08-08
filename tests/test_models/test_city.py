aa60f0d8381d399a603a26e81b2c521d02bd009d
Miguel22247 <nr2862002@gmail.com>
2021-08-08 20:01:38 -0300

    added comments to test_city

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -5,20 +5,20 @@ from models.city import City
 
 
 class test_City(test_basemodel):
-    """ """
+    """ tests the City class """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ initialization """
         super().__init__(*args, **kwargs)
         self.name = "City"
         self.value = City
 
     def test_state_id(self):
-        """ """
+        """ tests the state id """
         new = self.value()
         self.assertEqual(type(new.state_id), str)
 
     def test_name(self):
-        """ """
+        """ test the name """
         new = self.value()
         self.assertEqual(type(new.name), str)
