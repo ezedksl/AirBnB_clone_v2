9a646c97d2217348cba3b27facce970628e75151
Miguel22247 <minipachru@gmail.com>
2021-08-09 09:55:40 -0300

    added check storage

--- models/review.py
+++ models/review.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
+from sqlalchemy.sql.functions import user
+from models import place
 from models.base_model import BaseModel
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class Review(BaseModel):
-    """ Review classto store review information """
+    """ Review class to store review information """
     __tablename__ = reviews
-    place_id = Column(String(60), nullable=False)
-    user_id = Column(String(60), nullable=False)
-    text = Column(String(1024), nullable=False)
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        place_id = Column(String(60), nullable=False)
+        user_id = Column(String(60), nullable=False)
+        text = Column(String(1024), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
