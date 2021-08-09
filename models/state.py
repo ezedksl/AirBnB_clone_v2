ae299520929f47fbba96126bd3f38579d1cff7e1
Gabriel Prochnau <2288@holbertonschool.com>
2021-08-09 18:51:19 +0000

    Update city and state models

--- models/state.py
+++ models/state.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 import os
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
+    __tablename__ = "states"
     if os.getenv("HBNB_TYPE_STORAGE") == "db":
         name = Column(String(128), nullable=False)
+        cities = relationship("City", cascade="all, delete", backref="states")
     else:
         name = ""
+
+        @property
+        def cities(self):
+            """Getter for cities in the state"""
+            all_cities = models.storage.all(City)
+            state_cities = []
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    state_cities.append(city)
+            return state_cities
