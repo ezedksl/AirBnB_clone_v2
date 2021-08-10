d1c433728812e610ee60ec76aebdb9931c5319a4
Miguel22247 <nr2862002@gmail.com>
2021-08-10 20:15:44 -0300

    fix pep8

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -24,6 +24,7 @@ class Test_pep8(unittest.TestCase):
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
+
 class TestDBStorage(unittest.TestCase):
     '''this will test the database storage'''
 
