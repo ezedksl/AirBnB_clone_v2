aebba23892eb1d29b80f646af4f9fda80b4cda18
Miguel Pacheco <minipachru@gmail.com>
2021-08-10 13:29:13 -0300

    removed tests

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 """ Module for testing db storage """
-import unittest
-from models.engine.db_storage import DBStorage
-from models.base_model import BaseModel
-from models.user import User
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
-from models import storage
-from os import getenv
+from models.engine.db_storage import DBStorage
+from models.engine.file_storage import FileStorage
 import MySQLdb
+import unittest
 import pep8
+import os
+from os import getenv
+from models.base_model import BaseModel
+from models.user import User
 
 
 class Test_pep8(unittest.TestCase):
@@ -23,53 +24,3 @@ class Test_pep8(unittest.TestCase):
         result = pep8style.check_files(['models/engine/db_storage.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
-
-
-class Test_db_storage(unittest.TestCase):
-    """Tests the database storage"""
-    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != 'db',
-                     "can't run if storage is file")
-    def test_attributes_DBStorage(self):
-        """Tests for class attributes"""
-        self.assertTrue(hasattr(DBStorage, 'all'))
-        self.assertTrue(hasattr(DBStorage, 'new'))
-        self.assertTrue(hasattr(DBStorage, 'save'))
-        self.assertTrue(hasattr(DBStorage, 'reload'))
-        self.assertTrue(hasattr(DBStorage, 'delete'))
-        self.assertTrue(hasattr(DBStorage, '_DBStorage__engine'))
-        self.assertTrue(hasattr(DBStorage, '_DBStorage__session'))
-
-    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != 'db',
-                     "can't run if storage is file")
-    def test_new_DBStorage(self):
-        """Tests for new() method"""
-        nb = self.cursor.execute("SELECT COUNT(*) FROM states")
-        state = State(name="Oregon")
-        state.save()
-        nb1 = self.cursor.execute("SELECT COUNT(*) FROM states")
-        self.assertEqual(nb1 - nb, 0)
-
-    def test_all_DBStorage(self):
-        """Tests for the all method"""
-        state = State(name="California")
-        storage.new(state)
-        storage.save()
-        key = '{}.{}'.format(type(state).__name__, state.id)
-        dic = storage.all(State)
-        self.assertTrue(key in dic.keys())
-        state1 = State(name="Oregon")
-        storage.new(state1)
-        storage.save()
-        key1 = '{}.{}'.format(type(state1).__name__, state1.id)
-        dic1 = storage.all()
-        self.assertTrue(key in dic1.keys())
-        self.assertTrue(key1 in dic1.keys())
-        u = User(email="scoot@noot", password="scootnoot")
-        storage.new(u)
-        storage.save()
-        key2 = '{}.{}'.format(type(u).__name__, u.id)
-        dic2 = storage.all(User)
-        self.assertTrue(key2 in dic2.keys())
-        self.assertFalse(key1 in dic2.keys())
-        self.assertFalse(key in dic2.keys())
-        self.assertFalse(key2 in dic.keys())
