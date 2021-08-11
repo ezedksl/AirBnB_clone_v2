d5ee2ec2ac970167c1981ffc27e24a3bbaa7ff0a
Miguel22247 <minipachru@gmail.com>
2021-08-11 12:54:28 -0300

    remove tablename from if

--- models/city.py
+++ models/city.py
@@ -8,8 +8,8 @@ import os
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
+    __tablename__ = 'cities'
     if os.getenv('HBNB_TYPE_STORAGE') == "db":
-        __tablename__ = 'cities'
         namteab = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
 
