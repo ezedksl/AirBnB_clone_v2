d3530e77afd6d8786a906e6ede9cca6de72625cb
Eze <ezedksl@gmail.com>
2021-08-11 20:13:52 -0300

    changes

--- models/city.py
+++ models/city.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
+"""This is the city class"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-import os
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
+    """This is the class for City
+    Attributes:
+        state_id: The state id
+        name: input name
+    """
     __tablename__ = 'cities'
-    if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        places = relationship('Place', backref='cities', cascade='all, delete')
-    else:
-        name = ""
-        state_id = ""
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship('Place', backref='cities',
+                          cascade='all, delete-orphan')
--- models/state.py
+++ models/state.py
@@ -1,25 +1,26 @@
-#!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey
-import os
-from sqlalchemy.orm import relationship
-from models.city import City
+"""This is the state class"""
 import models
+from models.city import City
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+import os
+
 
 class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = "states"
-    if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        places = relationship('Place', backref='cities',
-                            cascade='all, delete-orphan')
+    """ This is the class for State
+    Attributes:
+        name: input name
+    """
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', backref='state',
+                              cascade='all, delete-orphan')
     else:
         @property
         def cities(self):
-            cities = []
-            for city in list(models.storage.all(City).values()):
-                if city.state_id == self.id:
-                    cities.append(city)
-            return cities
+            """ Getter attribute for FileStorage environment """
+            return [city for city in models.storage.all(City).values() if
+                    city.state_id == self.id]
