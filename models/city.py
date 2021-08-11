0215e77051568b75a075cac7c22e269c8ebb30a9
Miguel22247 <minipachru@gmail.com>
2021-08-11 16:51:58 -0300

    fixed city

--- models/city.py
+++ models/city.py
@@ -10,7 +10,7 @@ class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
     if os.getenv('HBNB_TYPE_STORAGE') == "db":
-        namteab = Column(String(128), nullable=False)
+        name= Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
 
         places = relationship("Place", cascade="all, delete", backref="cities")
