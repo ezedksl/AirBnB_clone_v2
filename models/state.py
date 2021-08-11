f37e654db82fe4d68469cc734e1bfd884a552928
Miguel22247 <minipachru@gmail.com>
2021-08-11 16:39:49 -0300

    removed property

--- models/state.py
+++ models/state.py
@@ -14,13 +14,3 @@ class State(BaseModel, Base):
         cities = relationship("City", cascade="all, delete", backref="states")
     else:
         name = ""
-
-        @property
-        def cities(self):
-            """Getter for cities in the state"""
-            all_cities = models.storage.all(City)
-            state_cities = []
-            for city in all_cities.values():
-                if city.state_id == self.id:
-                    state_cities.append(city)
-            return state_cities
