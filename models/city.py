5755ba9fc5eaed6f518a977a132857a99b328ec3
Miguel22247 <minipachru@gmail.com>
2021-08-11 10:45:57 -0300

    fixed pep8?

--- models/city.py
+++ models/city.py
@@ -8,12 +8,12 @@ import os
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
     if os.getenv('HBNB_TYPE_STORAGE') == "db":
+        __tablename__ = 'cities'
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         places = relationship('Place', backref='cities',
-                                cascade='all, delete-orphan')
+                            cascade='all, delete-orphan')
     else:
         name = ""
         state_id = ""
