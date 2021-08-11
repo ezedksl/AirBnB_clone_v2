dbdf9d5b016b925f45f5f026ce0879182c56bb6a
Miguel22247 <minipachru@gmail.com>
2021-08-11 10:33:24 -0300

    changed nullable on email

--- models/user.py
+++ models/user.py
@@ -10,7 +10,7 @@ class User(BaseModel):
     """This class defines a user by various attributes"""
     __tablename__ = 'user'
     if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        email = Column(String(128), nullable=True)
+        email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
