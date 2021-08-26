ad8135428d001dab2306d39886d16cf0a74eeb97
Eze <ezedksl@gmail.com>
2021-08-26 20:27:06 -0300

    checker, can i delete that redundant docstring?

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-"""Creates a .tgz backup of web_static/ contents.
-
-"""
+"""With Fabric, creates a .tgz backup of web_static/ contents."""
 from fabric import operations as op
 import datetime
 
 
 def do_pack():
-    """Uses fabric to pack web_static folder.
-
-    """
     op.local("mkdir versions &> /dev/null")
     now = datetime.datetime.now()
     version = "{}{}{}{}{}{}".format(now.year, now.month, now.day,
