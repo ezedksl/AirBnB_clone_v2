4cdf55d0afc54248de3e065f5ecb638c325881b5
Eze <ezedksl@gmail.com>
2021-08-26 19:28:18 -0300

    test

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-# script that set ups web servera
-sudo apt-get update
-sudo apt-get install -y nginx
-sudo ufw allow 'Nginx HTTP'
+# Script that sets a new server
+apt-get -y update
+apt-get -y install nginx
+ufw allow 'Nginx HTTP'
 mkdir -p /data/web_static/
 mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared
+mkdir -p /data/web_static/shared/
 echo "<html>
   <head>
   </head>
@@ -13,7 +13,7 @@ echo "<html>
     <p>Hello World</p>
   </body>
 </html>" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ data/web_static/current/
+ln -sf /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data
 sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 service nginx restart
